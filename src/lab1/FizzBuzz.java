9f06e321781571ab2b88555b84196e4f2a69ef24
bsu <cheb.su@mil.utoronto.ca>
2025-09-08 16:29:35 -0400

    refactored

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -32,22 +32,27 @@ class FizzBuzz {
 
         int n = 1;
         while (n <= 100) {
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = n % 3 == 0;
-            boolean divisibleBy5 = n % 5 == 0;
-
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
-                System.out.println("Fizz Buzz");
-            } else if (divisibleBy3) {
-                System.out.println("Fizz");
-            } else if (divisibleBy5) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(n);
-                n++;
-            }
+            n = doFizzBuzz(n);
         }
+    }
+
+    private static int doFizzBuzz(int n) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = n % 3 == 0;
+        boolean divisibleBy5 = n % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        } else if (divisibleBy3) {
+            System.out.println("Fizz");
+        } else if (divisibleBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(n);
+            n++;
+        }
+        return n;
     }
 
 }
