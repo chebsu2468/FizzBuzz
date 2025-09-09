0b36c26cbc243be8e965b52811830d798f6fa1d2
bsu <cheb.su@mil.utoronto.ca>
2025-09-09 11:15:04 -0400

    refactored the parameter orders

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -25,4 +25,19 @@ public class Multiples {
         System.out.println(count);
         return count;
     }
+    public static int multiples() {
+        int n = 1000;
+        int a = 3;
+        int b = 5;
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            boolean divisibleBy3 = i % a == 0;
+            boolean divisibleBy5 = i % b == 0;
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
 }
