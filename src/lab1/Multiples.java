6b10001b04e8590274d2b5b07a8d82311fee24f7
bsu <cheb.su@mil.utoronto.ca>
2025-09-08 20:04:52 -0400

    refactored the parameter orders

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -9,7 +9,7 @@ public class Multiples {
     }
 
     public static int multiples(int n, int a, int b) {
-        int count = 1;
+        int count = 0;
         for (int i = 1; i < n; i++) {
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % a == 0;
